2605082eace205bc1e05d2ac6690713415b2a467
MiaSong23 <46971141+MiaSong23@users.noreply.github.com>
2021-02-24 20:00:20 -0500

    Update Player.java

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -7,44 +7,57 @@
 
 package ca.sheridancollege.project;
 
-/**
- * A class that models each Player in the game. Players have an identifier, which should be unique.
- * @author dancye, 2018
- */
-public abstract class Player 
-{
-    private String playerID; //the unique ID for this player
+public class Player extends Actor {
+    //all players start with $300
+    private int money=300;
+    private int bet=0;
+    private boolean fold;
+    private String name;
     
-    /**
-     * A constructor that allows you to set the player's unique ID
-     * @param name the unique ID to assign to this player.
-     */
-    public Player(String name)
-    {
-        playerID= name;
-    }
-    
-    /**
-     * @return the playerID
-     */
-    public String getPlayerID() 
-    {
-        return playerID;
+    public Player(String name){
+        this.name=name;
     }
 
-    /**
-     * Ensure that the playerID is unique
-     * @param givenID the playerID to set
-     */
-    public void setPlayerID(String givenID) 
-    {
-        playerID = givenID;
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    // returns false if the bet is an invalid bet (IE. less than $1 or greater than the $s the player has)
+    public boolean bet(String givenBetString){
+        try{
+            int givenBetInt = Integer.parseInt(givenBetString);
+            if(givenBetInt>this.money||givenBetInt<1){
+                this.bet=0;
+                return false;
+            }else{
+                this.bet=givenBetInt;
+                this.money-=givenBetInt;
+                return true;
+            }
+        }catch(Exception e){
+            return false;
+        }
     }
     
-    /**
-     * The method to be instantiated when you subclass the Player class
-     * with your specific type of Player and filled in with logic to play your game.
-     */
-    public abstract void play();
+    public int getBet(){
+        return bet;
+    }
     
+    public void setFold(boolean fold){
+        this.fold=fold;
+    }
+    
+    public boolean getFold(){
+        return fold;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return name;
+    }
 }
